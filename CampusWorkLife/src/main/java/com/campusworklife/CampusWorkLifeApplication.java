8ef2ccf68366ef02a81f913824ced7225a6e2f8f
SeokGeunHo <45616952+seok6555@users.noreply.github.com>
2024-10-07 15:23:03 +0900

    Spring 프로젝트 세팅

---
+++ CampusWorkLife/src/main/java/com/campusworklife/CampusWorkLifeApplication.java
@@ -0,0 +1,13 @@
+package com.campusworklife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusWorkLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusWorkLifeApplication.class, args);
+	}
+
+}
---
+++ CampusWorkLife/src/test/java/com/campusworklife/CampusWorkLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campusworklife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusWorkLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
