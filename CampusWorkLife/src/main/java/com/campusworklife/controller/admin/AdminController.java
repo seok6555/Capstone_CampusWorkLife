15c32c7208c0715e60fa9cc0646c0a0842460351
Joo, Yeongeun <chrf07@gmail.com>
2024-11-18 14:42:03 +0900

    회원관리 페이지 '근로지' 표시 - OfficelistRepository 생성 - 컨트롤러, html 수정

--- CampusWorkLife/src/main/java/com/campusworklife/controller/admin/AdminController.java
+++ CampusWorkLife/src/main/java/com/campusworklife/controller/admin/AdminController.java
@@ -8,29 +8,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.campusworklife.dto.Member;
 import com.campusworklife.mapper.MemberMapper;
+import com.campusworklife.repository.OfficelistRepository;
+import com.campusworklife.entity.Officelist;
 
 @Controller
 @RequestMapping("admin")
 public class AdminController {
-	
+
 	@Autowired
 	private MemberMapper memberMapper;
-	
+
+	@Autowired
+	private OfficelistRepository officelistRepository;
+
 	@GetMapping("memAdminPage")
 	public String memAdminPage(Model model) {
 		String pageTitle = "회원관리";
 		List<Member> members = memberMapper.findAll();
+		List<Officelist> offices = officelistRepository.findAll();
 		model.addAttribute("members", members);
+		model.addAttribute("offices", offices);
 		return "admin/memAdminPage";
 	}
-	
+
 	@GetMapping("suggestionAdminPage")
 	public String suggestionAdminPage(Model model) {
 		String pageTitle = "게시판 관리";
 		model.addAttribute("message", pageTitle);
 		return "admin/suggestionAdminPage";
 	}
-	
+
 	@GetMapping("pageAdminPage")
 	public String pageAdminPage(Model model) {
 		String pageTitle = "페이지 관리";
